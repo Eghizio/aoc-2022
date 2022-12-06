82e93fff3ecae2ab4ef54fb020a77cf9948d216e
jakub-wasik <jakub.wasik@schibsted.com>
2022-12-06 07:23:44 +0100

    Day 6

---
+++ day-06/part-1.ts
@@ -0,0 +1,13 @@
+import { loadInput } from "../utils";
+
+
+const findPacketStart = (chars: string[], sequence: number) => {
+  let i = 0;
+  while (new Set(chars.slice(i, i + sequence)).size !== sequence) i++;
+  return i + sequence;
+};
+
+const input = loadInput("input");
+const chars = input.split("");
+
+console.log(findPacketStart(chars, 4));
